021c430d60e1d606aa173ec03529799d4679cb68
work <getnetabrham@gmail.com>
2022-02-01 16:59:29 +0300

    Blueprints and view

--- flasker/__init__.py
+++ flasker/__init__.py
@@ -22,5 +22,7 @@ def create_app(test_config=None):
     except:
         pass
     from . import db
+    from . import auth
     db.init_app(app)
+    app.register_blueprint(auth.bp)
     return app
